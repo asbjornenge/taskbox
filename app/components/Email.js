07f7c9b5e41f71ef7b50dfc779d5792250587c99
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-01-20 14:54:56 +0100

    Settings html in body

--- app/components/Email.js
+++ app/components/Email.js
@@ -4,7 +4,8 @@ var $     = React.DOM
 var Email = React.createClass({
     render : function() {
         var email = this.props.currentEmail
-        var body  = email.body.length > 1 ? email.body[1] : email.body[0]
+        var html  = email.body.length > 1
+        var body  = html ? email.body[1] : email.body[0]
         return $.div({
             key       : 'Email',
             className : 'Email MailBox'
@@ -15,7 +16,10 @@ var Email = React.createClass({
                 $.span({ key : 'from' }, email.from),
                 $.span({ key : 'date' }, email.date)
             ]),
-            $.div({ key : 'Body'}, body.content)
+            $.div({ 
+                key : 'Body',
+                dangerouslySetInnerHTML : { __html : body.content }
+            })
         ])
     }
 })
