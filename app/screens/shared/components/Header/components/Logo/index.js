76d6890baed66c0c3865ffd96fa085b064393d08
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-06 21:33:52 +0100

    Header work

--- app/screens/shared/components/Header/components/Logo/index.js
+++ app/screens/shared/components/Header/components/Logo/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import pony  from './graphics/pony.png'
+import style from './logo.styl'
 
 export default class Logo extends React.Component {
     render() {
         return (
             <div className="Logo">
+                <style>{style}</style>
                 <img src={pony} />
             </div>
         )
--- app/screens/shared/components/Header/index.js
+++ app/screens/shared/components/Header/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Logo  from './components/Logo'
+import React        from 'react'
+import Logo         from './components/Logo'
+import MailBoxLink  from './components/MailBoxLink'
+import TaskBoxLink  from './components/TaskBoxLink'
+import TweetBoxLink from './components/TweetBoxLink'
+import style        from './styles/index.styl'
 
 export default class Header extends React.Component {
     render() {
         return (
             <div className="Header">
+                <style>{style}</style>
+                <div className="screenLinks">
+                    <MailBoxLink />
+                    <TweetBoxLink />
+                    <TaskBoxLink />
+                </div>
                 <Logo />
             </div>
         )
