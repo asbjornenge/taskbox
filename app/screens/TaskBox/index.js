a7b3a8af23fe633ae0e2847a4d9031eb0bfe3342
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-12-11 14:40:14 +0100

    Some task action stuff

--- app/screens/TaskBox/index.js
+++ app/screens/TaskBox/index.js
@@ -1,13 +1,60 @@
-import React  from 'react'
-import Header from '../shared/components/Header'
+import React       from 'react'
+import { connect } from 'react-redux'
+import Header      from '../shared/components/Header'
 
-export default class TaskBox extends React.Component {
+class TaskBox extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            adding : false
+        }
+    }
     render() {
+        let tasks = this.props.tasks.map(task => {
+            return (
+                <div className="TaskBoxListItem">
+                    {task.name}
+                </div>
+            )
+        })
+        if (tasks.length == 0) tasks.push((
+            <div className="TaskBoxListItem empty">
+                No tasks
+            </div>
+        ))
+        let addInput, addSave
+        if (this.state.adding) {
+            addInput = (
+                <input ref="addInput" type="text" />
+            )
+            addSave = (
+                <button onClick={this.onSaveClick.bind(this)}>Save</button>
+            )
+        }
         return (
             <div>
                 <Header />
-                Tasks
+                <div className="taskActions">
+                    <button onClick={this.onAddClick.bind(this)}>+</button>
+                    {addInput}
+                    {addSave}
+                </div>
+                <div className="taskList">
+                    {tasks}
+                </div>
             </div>
         )
     }
+    onAddClick() {
+        this.setState({ adding : !this.state.adding })
+    }
+    onSaveClick() {
+        console.log('saving')
+    }
 }
+
+export default connect(state => {
+    return {
+        tasks : state.tasks
+    }
+})(TaskBox)
