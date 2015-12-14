56a50a8e939b563a11b3e6f3d8d1aa5078cd6c45
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-12-14 11:31:48 +0100

    A few updates

--- app/screens/MailBox/components/MailBoxItem.js
+++ app/screens/MailBox/components/MailBoxItem.js
@@ -8,8 +8,10 @@ export default class MailBoxItem extends React.Component {
         let participants = this.props.email.participants.map(participant => {
             return <span key={participant.email}>{participant.email}</span>
         })
+        let date = moment(this.props.email.last_message_timestamp*1000).format('MMM Do')
         return (
             <div className={classes}>
+                <div className="date">{date}</div>
                 <div className="participants">{participants}</div>
                 <div className="subject">{this.props.email.subject}</div>
                 <div className="summary">{this.props.email.snippet}</div>
