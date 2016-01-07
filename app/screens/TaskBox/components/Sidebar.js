cfb51e91695191828b85a1fabd486de6e9f79eae
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-01-07 11:29:55 +0100

    Sidebar groups

--- app/screens/TaskBox/components/Sidebar.js
+++ app/screens/TaskBox/components/Sidebar.js
@@ -4,12 +4,14 @@ import nav           from '../../shared/utils/nav'
 
 export default class Sidebar extends React.Component {
     render() {
-        let groups = this.props.tasks.reduce((groups, task) => { 
-            let taskgroup = task.group ? [task.group] : []
-            if (taskgroup.length == 0) return groups
-            if (groups.indexOf(taskgroup[0]) >= 0) return groups
-            return groups.concat(taskgroup)
-        },[])
+        let groups = this.props.tasks
+            .reduce((groups, task) => { 
+                let taskgroup = task.group ? [task.group] : []
+                if (taskgroup.length == 0) return groups
+                if (groups.indexOf(taskgroup[0]) >= 0) return groups
+                return groups.concat(taskgroup)
+            },[])
+            .filter(group => ['later','done'].indexOf(group) < 0)
         let groupItems = groups.map((group, index) => {
             return (
                 <li key={group+index} 
@@ -23,7 +25,11 @@ export default class Sidebar extends React.Component {
             <FlyoutSidebar show={this.props.show} animate={true}>
                 <div className="TaskBoxSidebar">
                     <ul className="groups">
+                        <li className="header">Groups</li>
                         {groupItems}
+                        <li className="separator">Status</li>
+                        <li className="later" onClick={this.setGroupFilter.bind(this, 'later')}>Later</li>
+                        <li className="later" onClick={this.setGroupFilter.bind(this, 'done')}>Done</li>
                     </ul>
                     <div className="settingsLink" onClick={nav.navigate.bind(undefined, '/settings')}>Settings</div>
                 </div>
