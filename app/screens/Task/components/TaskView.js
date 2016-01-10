fc52fb9bf4a1ae65752f03d6f138faf542b4eda3
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-01-10 21:18:25 +0100

    Linking links for task view

--- app/screens/Task/components/TaskView.js
+++ app/screens/Task/components/TaskView.js
@@ -3,11 +3,15 @@ import ReactMarkdown from 'react-markdown'
 
 export default class TaskView extends React.Component {
     render() {
+        let summary = this.props.task.summary || ''
+        let re = new RegExp('(http|ftp|https)://[a-z0-9\-_]+(\.[a-z0-9\-_]+)+([a-z0-9\-\.,@\?^=%&;:/~\+#]*[a-z0-9\-@\?^=%&;/~\+#])?', 'gi');
+
+        let summaryWithLinks = summary.replace(re, '[$&]($&)')
         return (
             <div className="TaskView">
                 <h1>{this.props.task.name}</h1>
                 <div className="summary">
-                    <ReactMarkdown source={this.props.task.summary || ''} />
+                    <ReactMarkdown source={summaryWithLinks} />
                 </div>
                 <div className="buttons">
                     <button onClick={this.props.removeTask.bind(this)}>Remove</button>
